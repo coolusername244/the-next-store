978b58e5ef6177479c94e183d1ef791f5f990649
Lee <leesheppard2404@gmail.com>
2024-05-13 17:12:15 +0200

    chore: Add Prisma client

---
+++ src/db/db.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from '@prisma/client';
+
+const prismaClientSingleton = () => new PrismaClient();
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma = global.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;

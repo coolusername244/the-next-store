860cde909744bbd65302b31edbe1becc029b2999
Lee <leesheppard2404@gmail.com>
2024-05-14 13:49:47 +0200

    chore: Refactor ProductForm component to handle both adding and updating products and give default values

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -5,8 +5,10 @@ import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
 import { formatCurrency } from '@/lib/formatters';
 import { Button } from '@/components/ui/button';
-import { addProduct } from '../../_actions/products';
+import { addProduct, updateProduct } from '../../_actions/products';
 import { useFormState, useFormStatus } from 'react-dom';
+import { Product } from '@prisma/client';
+import Image from 'next/image';
 
 const SubmitButton = () => {
   const { pending } = useFormStatus();
@@ -17,15 +19,26 @@ const SubmitButton = () => {
   );
 };
 
-const ProductForm = () => {
-  const [error, action] = useFormState(addProduct, {});
-  const [priceInCents, setPriceInCents] = useState<number>();
+const ProductForm = ({ product }: { product?: Product | null }) => {
+  const [error, action] = useFormState(
+    product == null ? addProduct : updateProduct.bind(null, product.id),
+    {},
+  );
+  const [priceInCents, setPriceInCents] = useState<number | undefined>(
+    product?.priceInCents,
+  );
 
   return (
     <form action={action} className="space-y-8">
       <div className="space-y-2">
         <Label htmlFor="name">Name</Label>
-        <Input type="text" id="name" name="name" required />
+        <Input
+          type="text"
+          id="name"
+          name="name"
+          required
+          defaultValue={product?.name}
+        />
         {error.name && <div className="text-destructive">{error.name}</div>}
       </div>
       <div className="space-y-2">
@@ -47,19 +60,35 @@ const ProductForm = () => {
       </div>
       <div className="space-y-2">
         <Label htmlFor="description">Description</Label>
-        <Input type="text" id="description" name="description" required />
+        <Textarea
+          id="description"
+          name="description"
+          defaultValue={product?.description}
+          required
+        />
         {error.description && (
           <div className="text-destructive">{error.description}</div>
         )}
       </div>
       <div className="space-y-2">
         <Label htmlFor="file">File</Label>
-        <Input type="file" id="file" name="file" required />
+        <Input type="file" id="file" name="file" required={product == null} />
+        {product != null && (
+          <div className="text-muted-foreground">{product.filePath}</div>
+        )}
         {error.file && <div className="text-destructive">{error.file}</div>}
       </div>
       <div className="space-y-2">
         <Label htmlFor="image">Image</Label>
-        <Input type="file" id="image" name="image" required />
+        <Input type="file" id="image" name="image" required={product == null} />
+        {product != null && (
+          <Image
+            src={product.imagePath}
+            alt={product.name}
+            width={400}
+            height={400}
+          />
+        )}
         {error.image && <div className="text-destructive">{error.image}</div>}
       </div>
       <SubmitButton />
